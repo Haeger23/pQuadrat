f3b6edf00ddebc369f47504aa4518fdfae8994a1
Friedolin <friedolinfoerder@gmx.de>
2012-11-20 16:02:53 +0100

    resolver changed: A view can be shown without presenter

--- core/p_squared_resolver.rb
+++ core/p_squared_resolver.rb
@@ -11,52 +11,82 @@ class PSquaredResolver < Sinatra::Base
       currentLocals = @locals
       options = args.extract_options!
       options.merge!(:layout => false, :locals => @locals.merge(locals))
-      out = erb((@presenter+"/"+view.to_s).to_sym, options)
+      out = erb((@presenter+"/"+view.to_s+"."+@format).to_sym, options)
       @presenter = currentPresenter
       @locals = currentLocals
-      return out
+      out
+    end
+    def js(*args)
+      @js ||= []
+      newJs = []
+      args.each do |file|
+        if(file[0] == "/")
+          src = "#{settings.public_folder}/#{file[1..-1]}.js"
+        else
+          src = "#{settings.views}/#{@presenter}/#{file}.js"
+        end
+        newJs << "<script type='text/javascript' src='#{src}'></script>"
+      end
+      @js = newJs | @js
+      @js.join("\n")
+    end
+    def css(*args)
+      out = ""
+      args.each do |file|
+        out << "<link rel='stylesheet' type='text/css' href='#{settings.public_folder}/#{file}.css'></link>\n"
+      end
+      out
     end
   end
 
 protected
   def resolve(presenter, action, *args)
+    # find requested format
+    request.preferred_type(%w[text/html application/json application/x-json text/json text/x-json application/xml text/xml])
+    if request.accept? "text/html"
+      @format = "html"
+    elsif request.accept? "application/json" or request.accept? "application/x-json" or request.accept? "text/json" or request.accept? "text/x-json"
+      @format = "json"
+    elsif request.accept? "application/xml" or request.accept? "text/xml"
+      @format = "xml"
+    else
+      @format = "html"
+    end
+
     options = args.extract_options!
     require_relative "../presenters/#{presenter}"
-    presenterInstance = Object.const_get(presenter.capitalize+"Presenter").new
+    if Object.respond_to?((presenter.capitalize+"Presenter").to_sym)
+      presenterInstance = Object.const_get(presenter.capitalize+"Presenter").new
+      @locals = presenterInstance.view
+    else
+      presenterInstance = nil
+      @locals = {}
+    end
     @presenter = presenter
-    @locals = presenterInstance.view
 
-    begin
-      if presenterInstance.respond_to?(action.to_sym)
+    if presenterInstance.respond_to?(action.to_sym)
+      begin
         method = presenterInstance.method(action.to_sym)
         method.call(*args)
+      rescue ResolverStoppedError
+        pass
+        return
       end
-    rescue ResolverStoppedError
-      pass
-      return
+    end
+
+    actionSym = (action+"_"+@format).to_sym
+    if presenterInstance.respond_to?(actionSym)
+      begin
+        method = presenterInstance.method(actionSym)
+        method.call(*args)
+      rescue ResolverStoppedError
+        pass
+        return
+      end
     end
 
     begin
-      request.preferred_type(%w[text/html application/json application/x-json text/json text/x-json application/xml text/xml])
-      if request.accept? "text/html"
-        format = "html"
-      elsif request.accept? "application/json" or request.accept? "application/x-json" or request.accept? "text/json" or request.accept? "text/x-json"
-        format = "json"
-      elsif request.accept? "application/xml" or request.accept? "text/xml"
-        format = "xml"
-      else
-        format = "html"
-      end
-
-      actionSym = (action+"_"+format).to_sym
-      if presenterInstance.respond_to?(actionSym)
-        method = presenterInstance.method(actionSym)
-        method.call(*args)
-      end
-      erb((@presenter+"/"+action+"."+format).to_sym, :locals => @locals, :layout => ("layout."+format).to_sym)
-    rescue ResolverStoppedError
-      pass
-      return
+      erb((@presenter+"/"+action+"."+@format).to_sym, :locals => @locals, :layout => ("layout."+@format).to_sym)
     rescue Errno::ENOENT => e
       puts "view '#{e.message.split("/")[-2..-1].join("/")}' not available"
     end
