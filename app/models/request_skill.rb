234e9f952af29caa6bd606af5f63446da0a83456
Friedolin <friedolinfoerder@gmx.de>
2012-12-13 13:13:26 +0100

    model files updated

--- app/models/request_skill.rb
+++ app/models/request_skill.rb
@@ -6,7 +6,7 @@ class RequestSkill < Model
   belongs_to :project_skill
 
   validates_presence_of :request, :project_skill, :weight
-  validates_numericality_of :weight, :only_integer => true
+  validates_numericality_of :weight, :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
   validates_uniqueness_of :request_id, :scope => :project_skill_id, :message => "The request has already this skill"
 
 end
--- app/models/user_project_skill.rb
+++ app/models/user_project_skill.rb
@@ -8,7 +8,7 @@ class UserProjectSkill < Model
   delegate :skill, :to => :project_skill
 
   validates_presence_of :user, :project_skill, :weight
-  validates_numericality_of :weight, :only_integer => true
+  validates_numericality_of :weight, :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100
   validates_uniqueness_of :user_id, :scope => :project_skill_id, :message => "The user has already this skill of the project"
 
 end
